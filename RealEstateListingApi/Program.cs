c02b17c9969a53e6998845aae2cfbff8a1d0f4be
Patricio Chamorro <patricio.chamorro@innovation-box.tech>
2025-12-11 15:59:00 -0500

    update procfile

--- RealEstateListingApi/Program.cs
+++ RealEstateListingApi/Program.cs
@@ -54,9 +54,10 @@ using (var scope = app.Services.CreateScope())
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Real Estate Listing API V1"));
+    
 }
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Real Estate Listing API V1"));
 
 app.UseAuthorization();
 app.MapControllers();
